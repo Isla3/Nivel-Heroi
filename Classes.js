409671588eb62c24c79fe68fa991b203316a087d
Isla3 <il7771@outlook.com>
2024-01-21 15:14:25 -0300

    Terceiro commit

---
+++ Classes.js
@@ -0,0 +1,22 @@
+class heroi{
+    constructor (nome, idade,tipo){
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+}
+
+function atacar(){
+    if (tipo==="mago"){
+        console. log("Mago atacou usando magia")
+        }
+        else if (tipo==="guerreiro"){
+            console.log("Guerreiro atacou usando espada")
+        }
+else if (tipo==="monge"){
+    console.log("Monge atacou usando marciais")
+        }
+    else if (tipo==="ninja"){
+        console.log("Ninja atacou usando shuriken")
+}
+}
